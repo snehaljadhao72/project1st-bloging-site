149564a3824dacf41454b3433b3bb6e4c13a1897
snehal jadhao <snehaljadhao8805@gmail.com>
2022-06-26 21:41:02 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const middleware = require("../middleware/middleware")
+//const middleware = require("../middleware/middleware")
 const authorController = require('../controllers/authorCotroller');
 const blogController = require('../controllers/blogControllers')
 const loginCotroller = require("../controllers/loginController")
 
 /**------------------------------------Authorization--------------------------------------------- */
 
-router.post('/authors', authorController.createAuthor)
+// router.post('/authors', authorController.createAuthor)
 
-router.post("/login", loginCotroller.loginAuthor)
+// router.post("/login", loginCotroller.loginAuthor)
 
-router.post('/blogs', middleware.authenticate, blogController.createBlog)
+// router.post('/blogs', middleware.authenticate, blogController.createBlog)
 
-router.get('/blogs', middleware.authenticate, blogController.getBlogs)
+// router.get('/blogs', middleware.authenticate, blogController.getBlogs)
 
-router.put('/blogs/:blogId', middleware.authenticate, middleware.authorise, blogController.updateBlog)
+// router.put('/blogs/:blogId', middleware.authenticate, middleware.authorise, blogController.updateBlog)
 
-router.delete('/blogs/:blogId', middleware.authenticate, middleware.authorise, blogController.deleteByParams)
+// router.delete('/blogs/:blogId', middleware.authenticate, middleware.authorise, blogController.deleteByParams)
 
-router.delete('/blogs', middleware.authenticate, middleware.authorise, blogController.deleteByQuery)
+// router.delete('/blogs', middleware.authenticate, middleware.authorise, blogController.deleteByQuery)
 
 /**----------------------------------------------------------------------------------------------- */
 
 // router.post('/authors', authorController.createAuthor)
 
-// router.post('/blogs',blogController.createBlog)
+ router.post('/blogs',blogController.createBlog)
 // router.get('/blogs',blogController.getBlogs)
 
 // router.put('/blogs/:blogId',blogController.updateBlog)
